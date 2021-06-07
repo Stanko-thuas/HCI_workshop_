b6fb781487341b29353f828290b914abae34c0ff
Stanko99 <stanko.ivanov99@hotmail.com>
2021-06-07 19:04:11 +0200

    Playing with animations

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,12 +1,16 @@
 
 
-// // changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
+// changing individual properties with code and using setInterval
+ var rotationSpeedX = 0.02;
+ var rotationSpeedY = 0.005;
+ var myOtherBox = document.getElementById('myOtherBox');
 
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeed;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
+ function spin(){
+ 	myOtherBox.object3D.rotation.x += -rotationSpeedX;
+    myOtherBox.object3D.rotation.y += -rotationSpeedY;
 
-// setInterval(spin, 16); //equivalent to 60 fps
+ 	console.log(myOtherBox.object3D.rotation.x);
+    console.log(myOtherBox.object3D.rotation.y);
+ }
+
+ setInterval(spin, 16); //equivalent to 60 fps
